30f05ef1d784c70743fd7957abfaf0e92c51c9a3
BazhenovYN <BazhenovYN@gmail.com>
2023-02-18 14:22:56 +0300

    feat: done morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,28 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function morseToBin(morse) {
+    const morseBin = {};
+    for (const [key, value] of Object.entries(morse)) {
+        const bin = key.replace(/[.-]/g, (match) => match === '.' ? '10' : '11');
+        morseBin[bin.padStart(10, '0')] = value;
+    }
+    return morseBin;
+}
+
+function decodeWord(wordBin, alphabet) {
+    return wordBin.match(/.{1,10}/g).reduce((acc, charBin) => acc + alphabet[charBin], '');
+}
+
 function decode(expr) {
-    // write your solution here
+    const morseBin = morseToBin(MORSE_TABLE);
+    const words = expr.split('**********');
+    const sentence = words.reduce((acc, wordBin) => {
+        const word = decodeWord(wordBin, morseBin);
+        acc.push(word);
+        return acc;
+    }, []);
+    return sentence.join(' ');
 }
 
 module.exports = {
